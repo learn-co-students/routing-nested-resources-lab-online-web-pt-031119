41faa8732da521dec5645fa2828d4183ae6cd960
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-09-03 22:17:12 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists
+  
+  resources :artists do 
+    #nested routes for songs
+    resources :songs, only: [:show, :index]
+  end
+
   resources :songs
 end
